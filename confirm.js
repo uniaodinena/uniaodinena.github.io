e1144c252360d15916ce31af90309ab1e84e73b1
Igor Duca <igorducca@gmail.com>
2020-07-27 22:52:04 -0300

    :gear: Initial commit

---
+++ confirm.js
@@ -0,0 +1,22 @@
+function register(){
+
+    var name = document.getElementById('name').value
+    var email = document.getElementById('email').value
+
+    if(!name) alert("Você precisa me dizer seu nome!")
+    if(!email) alert("Você precisa me dizer seu e-mail!")
+
+    var registerStatus = document.getElementById('registerStatus')
+    var usernameText = document.getElementById('usernameText')
+    var registerButton = document.getElementById('register')
+    var email_warning = document.getElementById('email_warning')
+
+    registerButton.hidden = true;
+
+    registerStatus.innerHTML = 'Registrado(a) com sucesso!'
+    usernameText.innerHTML = `${name}, valeu por sua inscrição! 💥`
+
+    email_warning.hidden = false;
+
+    document.body.style.background = 'url(https://media.giphy.com/media/MXQnyEQwBJ6eTj90L5/giphy.gif)'
+}
